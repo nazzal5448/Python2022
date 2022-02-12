947ef3d24134f41a10b14e3a4b0cc8537945afbf
nazzal5448 <98973132+nazzal5448@users.noreply.github.com>
2022-02-12 07:16:50 +0500

    Create Class3.py

---
+++ Class3.py
@@ -0,0 +1,13 @@
+employees =('Farah', 'Ali','Asad','Shameer',
+    'Subhan','Salar','Bafsa','Sana','Hoora','Ahad','Sana','Maryam')
+    
+print(type(employees))
+
+flavours = {'orange','pineapple','grapes','pepsi','pakola','reditrish'}
+print(type(flavours))
+
+medical_tools = {'ventilator','ct scan','defibrillator','ecg machine',
+    'mri'
+    }
+    
+print(type(medical_tools))
