497589902a3714a94c2e8648f8edb34a6ebb6873
nazzal5448 <98973132+nazzal5448@users.noreply.github.com>
2022-02-15 17:41:43 +0500

    Add files via upload

---
+++ class5.py
@@ -0,0 +1,27 @@
+print('=======Task1=======')
+
+name= input('Please enter your name: ')
+height= float(input("please enter your height: "))
+contact= int(input('Please enter your phone number: '))
+print(name, height, contact)
+
+print('========Task 2========')
+#ONLINE ADMISSION FORM FOR SKILLVENTON
+
+name = input ('Please enter your name: ')
+f_name= input('Please enter your father/guardian name: ')
+email= input('Please enter your email address: ')
+contact_no= int(input('Please enter your phone number: '))
+skill= input('Do you already have any skill? yes/no : ')
+
+print('Thanks for giving us your time. ')
+
+print('========Task 3 ========')
+
+username= input('Please enter your username or email address: ')
+password= input('Please enter your password : ')
+
+print('Successfully logged in')
+
+
+
