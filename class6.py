44b4c344fc514babc90d69ae45cfaa1ced59c5ea
nazzal5448 <98973132+nazzal5448@users.noreply.github.com>
2022-02-18 22:46:26 +0500

    Add files via upload

---
+++ class6.py
@@ -0,0 +1,33 @@
+print('=============Task 1=============')
+email = input('Enter your email address: ')
+print(email.endswith('.com'))
+
+print('===========Task 2===========')
+name=input('Please tell me your name: ')
+print('Hello ', name.title(), ', I hope you are fine. please let me know how can u help you?')
+user= input(' Do you need any help? ')
+print (user.find('thanks'))
+print(user.find('no thanks'))
+print (user.find("i don't need any help."))
+
+#for true
+
+print('okay, good to see you. stay connected.')
+print(user.find('yes'))
+print (user.find('yes i need your help'))
+ #for true
+ 
+print('we will resolve your problem.')
+ 
+print('==============task 3=============')
+ 
+url=input('Enter your url: ')
+print (url.startswith('https://www.'))
+
+#for true
+
+print(url.replace('https://www.' ,name))
+
+ 
+ 
+ 
