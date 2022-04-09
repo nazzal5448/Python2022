40256b2a200742894a7411d7a0d3531709573ce8
Nazzal Kausar <98973132+nazzal5448@users.noreply.github.com>
2022-04-09 18:20:32 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,23 @@
+a= [['happy','BirtHday','to','yoU'],['this','Is','MY','BdaY','Month'],['wish','me','HappY','BirtHday']]
+
+b=[]
+c=[]
+d=[]
+e=[]
+for l in a:
+    if l==a[0]:
+        for l1 in l:
+            b.append(l1.upper())
+        e.append(b)
+    elif l==a[1]:
+        for l1 in l:
+            c.append(l1.upper())
+        e.append(c)
+    elif l==a[2]:
+        for l1 in l:
+            d.append(l1.upper())
+        e.append(d)
+    else:
+        pass
+
+print(e)
