d5b15f1581b36609bdff72c7897c15fe3172ed8f
Nazzal Kausar <98973132+nazzal5448@users.noreply.github.com>
2022-04-09 18:23:39 +0500

    Update task.py

--- task.py
+++ task.py
@@ -4,20 +4,21 @@ b=[]
 c=[]
 d=[]
 e=[]
-for l in a:
-    if l==a[0]:
-        for l1 in l:
-            b.append(l1.upper())
-        e.append(b)
-    elif l==a[1]:
-        for l1 in l:
-            c.append(l1.upper())
-        e.append(c)
-    elif l==a[2]:
-        for l1 in l:
-            d.append(l1.upper())
-        e.append(d)
-    else:
-        pass
+def upper(l):
+    for l in a:
+        if l==a[0]:
+            for l1 in l:
+                b.append(l1.upper())
+            e.append(b)
+        elif l==a[1]:
+            for l1 in l:
+                c.append(l1.upper())
+            e.append(c)
+        elif l==a[2]:
+            for l1 in l:
+                d.append(l1.upper())
+            e.append(d)
+        else:
+            pass
 
-print(e)
+upper(a)
